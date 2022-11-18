50b199091217f690af5ea830cadaf3864cd999ed
levus <levusachev082@outlook.com>
2022-11-19 00:40:01 +0400

    task 6_2a is complete

--- 6_partition/6_2a exercise.py
+++ 6_partition/6_2a exercise.py
@@ -1,7 +1,26 @@
 # https://pyneng.readthedocs.io/ru/latest/exercises/06_exercises.html
 # 6.2a
 
-entered_address = input("Please, enter the address at format '192.168.0.1': ")
+while True:
+    entered_address = input("Please, enter the address at format '192.168.0.1': ")
+    i = 0
+    for dot in entered_address:
+        if dot == '.':
+            i += 1
+    if i != 3:
+        entered_address = input("Please, enter the address at format '192.168.0.1': ")
+
+    splitted_ip_address = entered_address.split('.')
+
+    try:
+        a = int(splitted_ip_address[0]) * int(splitted_ip_address[1]) * int(splitted_ip_address[2]) * \
+            int(splitted_ip_address[3])
+    except ValueError:
+        print('IP address must be contains only numbers')
+    else:
+        break
+
+# entered_address = input("Please, enter the address at format '192.168.0.1': ")
 splitted_ip_address = entered_address.split('.')
 a1, a2, a3, a4 = [
     int(splitted_ip_address[0]),
