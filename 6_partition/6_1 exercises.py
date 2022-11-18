31899e3e05234945f871f1d92907f975c2e03259
levus <levusachev082@outlook.com>
2022-11-18 19:09:25 +0400

    complete task 6.1

--- 6_partition/6_1 exercises.py
+++ 6_partition/6_1 exercises.py
@@ -1,2 +1,10 @@
 # https://pyneng.readthedocs.io/ru/latest/exercises/06_exercises.html
 # 6.1
+mac = ["aabb:cc80:7000", "aabb:dd80:7340", "aabb:ee80:7000", "aabb:ff80:7000"]
+result = []
+i = 0
+while i <= len(mac) - 1:
+    result.append(mac[i].replace(':', '.'))
+    i += 1
+print(result)
+
