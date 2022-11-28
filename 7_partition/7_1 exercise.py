2ed2b6534dc434f8a9330ed8d60caaedc7a8e73a
levus <levusachev082@outlook.com>
2022-11-28 22:17:46 +0300

    task 7.1 is done, py's file 7.2 added

--- 7_partition/7_1 exercise.py
+++ 7_partition/7_1 exercise.py
@@ -1,2 +1,17 @@
 # https://pyneng.readthedocs.io/ru/latest/exercises/07_exercises.html
 # 7.1
+
+output = "\n{:25} {}" * 5
+
+with open("ospf.txt", "r") as f:
+    for line in f:
+        route = line.replace(",", " ").replace("[", "").replace("]", "")
+        route = route.split()
+
+        print(output.format(
+                "Prefix", route[1],
+                "AD/Metric", route[2],
+                "Next-Hop", route[4],
+                "Last update", route[5],
+                "Outbound Interface", route[6],
+        ))
