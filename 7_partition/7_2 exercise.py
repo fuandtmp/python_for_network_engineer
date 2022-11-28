aa86cfeb9db46aac7e2d967a96052efa03533752
levus <levusachev082@outlook.com>
2022-11-28 22:36:39 +0300

    task 7.2 is done, added config_sw1.txt

--- 7_partition/7_2 exercise.py
+++ 7_partition/7_2 exercise.py
@@ -1,2 +1,11 @@
 # https://pyneng.readthedocs.io/ru/latest/exercises/07_exercises.html
 # 7.2
+
+from sys import argv
+
+file = argv[1]
+
+with open(file) as config:
+    for line in config:
+        if not '!' in line:
+            print(line, end=' ')
