52c64cdbd7bae19c8665825503f209746b22d15c
levus <levusachev082@outlook.com>
2022-11-29 17:39:17 +0300

    task 7_2a is done

--- 7_partition/7_2 exercise.py
+++ 7_partition/7_2 exercise.py
@@ -1,11 +1,15 @@
 # https://pyneng.readthedocs.io/ru/latest/exercises/07_exercises.html
-# 7.2
+# 7.2a
 
 from sys import argv
 
 file = argv[1]
 
+ignore = ["duplex", "alias", "configuration"]
+
 with open(file) as config:
     for line in config:
-        if not '!' in line:
-            print(line, end=' ')
+        words = line.split()
+        cross_words = set(words) & set(ignore)
+        if not line.startswith("!") and not cross_words:
+            print(line.rstrip())
