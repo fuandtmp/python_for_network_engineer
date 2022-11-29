2298757f276a11f20b6cab666b0cbfc524c304a7
levus <levusachev082@outlook.com>
2022-11-29 18:14:39 +0300

    task 7_2b is done and update .gitignore

--- 7_partition/7_2 exercise.py
+++ 7_partition/7_2 exercise.py
@@ -4,12 +4,17 @@
 from sys import argv
 
 file = argv[1]
+ext_file = argv[2]
 
 ignore = ["duplex", "alias", "configuration"]
+# f = open('test.txt', 'w')
 
-with open(file) as config:
+with open('config_sw1.txt') as config:
     for line in config:
         words = line.split()
         cross_words = set(words) & set(ignore)
         if not line.startswith("!") and not cross_words:
-            print(line.rstrip())
+            newlines = line.rstrip() + '\n'
+            ext_file.writelines(newlines)
+
+ext_file.close()
