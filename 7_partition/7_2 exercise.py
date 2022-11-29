32111eff0790dea68221b0d8b64c046f63ef6533
levus <levusachev082@outlook.com>
2022-11-29 18:28:21 +0300

    fix 7_2b task use for example answers from https://github.com/natenka/pyneng-answers/

--- 7_partition/7_2 exercise.py
+++ 7_partition/7_2 exercise.py
@@ -1,20 +1,16 @@
 # https://pyneng.readthedocs.io/ru/latest/exercises/07_exercises.html
-# 7.2a
+# 7.2b
 
 from sys import argv
 
-file = argv[1]
-ext_file = argv[2]
+file, ext_file = argv[1], argv[2]
 
 ignore = ["duplex", "alias", "configuration"]
-# f = open('test.txt', 'w')
+# f = open('test.txt', 'w') <<-- For tests
 
-with open(file) as config:
+with open(file) as config, open(ext_file, 'w') as dst:
     for line in config:
         words = line.split()
         cross_words = set(words) & set(ignore)
         if not line.startswith("!") and not cross_words:
-            newlines = line.rstrip() + '\n'
-            ext_file.writelines(newlines)
-
-ext_file.close()
+            dst.write(line)
