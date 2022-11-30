c3622d22d3d71165235c5e823787d03a616b30d6
levus <levusachev082@outlook.com>
2022-11-30 13:42:55 +0300

    task 7.3b is done

--- 7_partition/7_3 exercise.py
+++ 7_partition/7_3 exercise.py
@@ -1,10 +1,14 @@
 # https://pyneng.readthedocs.io/ru/latest/exercises/07_exercises.html
-# 7.3
+# 7.3b
 
+mac_table = []
 
 with open('mac_table.txt') as config:
     for line in config:
         words = line.split()
         if words and words[0].isdigit():
             vlan, mac, _, interface = words
-            print(f'{vlan:9}{mac:20}{interface}')
+            mac_table.append([int(vlan), mac, interface])
+
+for vlan, mac, interface in sorted(mac_table):
+    print(f'{vlan:<9}{mac:20}{interface}')
