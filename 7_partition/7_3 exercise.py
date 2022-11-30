964cf7cbc1bf1483485a77d9b491208abf940564
levus <levusachev082@outlook.com>
2022-11-30 13:32:51 +0300

    task 7.3 right version

--- 7_partition/7_3 exercise.py
+++ 7_partition/7_3 exercise.py
@@ -1,15 +1,10 @@
 # https://pyneng.readthedocs.io/ru/latest/exercises/07_exercises.html
 # 7.3
 
-template = "{0:9} {1:20} {2}"
 
 with open('mac_table.txt') as config:
     for line in config:
         words = line.split()
-        try:
-            mac_address = str(words[1])
-            first_octet_mac = int(mac_address[:4], 16)
-            if type(first_octet_mac) == int:
-                print(template.format(words[0], words[1], words[3]))
-        except:
-            pass
+        if words and words[0].isdigit():
+            vlan, mac, _, interface = words
+            print(f'{vlan:9}{mac:20}{interface}')
